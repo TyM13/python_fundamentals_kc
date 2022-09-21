90934c0919be7052bc68c7b16428c0941395f976
Your Name <you@example.com>
2022-09-20 19:34:59 -0600

    part 3

--- fundamentals.py
+++ fundamentals.py
@@ -1,25 +1,35 @@
-num = 5
+num = -5
 string = 'im a string'
 boolean = False
 
-print(num)
-print(string)
-print(boolean)
-print(num, string, boolean)
+# print(num)
+# print(string)
+# print(boolean)
+# print(num, string, boolean)
 
 
-if(num > 10):
-    print('that is larger than 10')
-else:
-    print('that is not larger than 10')
+# if(num > 10):
+#     print('that is larger than 10')
+# else:
+#     print('that is not larger than 10')
 
 
 
-if(num < 0 and boolean == True): 
-    print('negative and true')
-elif(num > 0 and boolean == False):
-    print('posative false')
-elif(num > 100 or boolean == True):
-    print('larger or true')
-else:
-    ('i dont know')
+# if(num < 0 and boolean == True): 
+#     print('negative and true')
+# elif(num > 0 and boolean == False):
+#     print('posative false')
+# elif(num > 100 or boolean == True):
+#     print('larger or true')
+# else:
+#     print("i don't know")
+
+
+array_of_strings = ['one','two','three']
+array_of_numbers = [1,2,3]
+
+for i in range(1):
+    print(array_of_strings)
+
+    for num in array_of_numbers:
+        print('look at this number', num)
